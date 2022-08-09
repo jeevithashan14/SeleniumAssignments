e2fe6ddee49e2c49b0465ef509a1f00acdf9b8ad
jeevithashan14 <110905574+jeevithashan14@users.noreply.github.com>
2022-08-09 20:45:49 +0530

    Update ConvertANegativeNumberToPositiveNumber.java

--- week1/day1/ConvertANegativeNumberToPositiveNumber.java
+++ week1/day1/ConvertANegativeNumberToPositiveNumber.java
@@ -17,21 +17,22 @@ public class ConvertANegativeNumberToPositiveNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-       // Initialize an integer with a negative number
-		int number = -100;
+                // Initialize an integer with a negative number
+		int givenNumber = -100;
+		int positiveNumber;
 		
 		//Check if a number is positive, print "The number is positive
-		if(number > 0) {
-			System.out.println("Value of number " +number+ " is greater than 0 so it is Positive");
+		if(givenNumber > 0) {
+			System.out.println("Value of number " +givenNumber+ " is greater than 0 so it is Positive");
 		}
 		
 		//Check if a number is negative, print "The number is negative"
 		//Convert the number to a positive number
 		//Print the converted positive number
-		else if (number < 0 ) {
-		System.out.println("Value of number " +number+ " is less than 0 so it is Negative");
-		number = number * -1;
-		System.out.println("Converted Positive Number: " +number);
+		else if (givenNumber < 0 ) {
+		System.out.println("Value of number " +givenNumber+ " is less than 0 so it is Negative");
+		positiveNumber = givenNumber * -1;
+		System.out.println("Converted Positive Number: " +positiveNumber);
 		}
 		
 	}
